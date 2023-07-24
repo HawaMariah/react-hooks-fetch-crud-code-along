57b1c9f2474b51bada845cdb357425dd99e8b0de
Hawa <hawamaria33@gmail.com>
2023-07-24 22:54:58 +0300

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,23 @@
+// import React, { useState } from "react";
+// import ShoppingList from "./ShoppingList";
+// import Header from "./Header";
+
+// function App() {
+//   const [isDarkMode, setIsDarkMode] = useState(false);
+
+//   function handleDarkModeClick() {
+//     setIsDarkMode((isDarkMode) => !isDarkMode);
+//   }
+
+//   return (
+//     <div className={"App " + (isDarkMode ? "dark" : "light")}>
+//       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
+//       <ShoppingList />
+//     </div>
+//   );
+// }
+
+// export default App;
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
